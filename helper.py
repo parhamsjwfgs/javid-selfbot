34a90a6d801be1a896689cafa7ef71933c3604d4
Deploy <deploy@bot.local>
2026-09-03 00:12:03 +0000

    Fix all BOT_TOKEN placeholders

--- helper.py
+++ helper.py
@@ -6,7 +6,7 @@ from telegram.constants import ParseMode
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "ع؟"
+BOT_TOKEN = "8994843551:AAFbF5KtXf-1RQ0PN5woZUPyZFP6517OAaI"
 PANEL_IMAGE_URL = "https://t.me/Sspideermman2/230" 
 
 def create_button(text, callback_data, style=None):
